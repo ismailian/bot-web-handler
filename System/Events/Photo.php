66c87e4998ddb8a161679f6398f70858eb6110b6
Ismail Aatif <techcyber598@gmail.com>
2023-12-27 20:58:47 +0100

    updated Photo attribute

--- System/Events/Photo.php
+++ System/Events/Photo.php
@@ -14,6 +14,7 @@ class Photo implements IEvent
      */
     public function apply(array $event): bool
     {
-        return isset($event['data']['message']) && isset($event['data']['message']['photo']);
+        $key = isset($event['data']['edited_message']) ? 'edited_message' : 'message';
+        return isset($event['data'][$key]) && isset($event['data'][$key]['photo']);
     }
 }
