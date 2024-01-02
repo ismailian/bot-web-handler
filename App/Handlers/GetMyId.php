e4f9760d86e7ef6da3bc32cc2fa58ac8a4e41823
ismailaatif <ismailaatif@oggvo.com>
2024-01-02 17:01:48 +0100

    added basic handler

---
+++ App/Handlers/GetMyId.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TeleBot\App\Handlers;
+
+use TeleBot\System\BaseEvent;
+use TeleBot\System\Events\Message;
+use GuzzleHttp\Exception\GuzzleException;
+
+class GetMyId extends BaseEvent
+{
+
+    /**
+     * handle all incoming messages
+     *
+     * @return void
+     * @throws GuzzleException
+     */
+    #[Message]
+    public function index(): void
+    {
+        try {
+            $botToken = getenv('TG_BOT_TOKEN', true);
+            $chatId = $this->event['data']['message']['chat']['id'];
+            $userId = $this->event['data']['message']['chat']['id'];
+            $message = "Your user ID: <strong>{$userId}</strong>\nCurrent chat ID: <strong>{$chatId}</strong>";
+
+            $this->client->post("/bot{$botToken}/sendMessage", [
+                'json' => [
+                    'chat_id' => $this->event['data']['message']['chat']['id'],
+                    'text' => $message,
+                    'parse_mode' => 'html'
+                ]
+            ]);
+        } catch (\Exception $ex) {}
+    }
+
+}
