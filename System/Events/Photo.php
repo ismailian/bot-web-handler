f21097fd8c8f1842a5d4a414cacbb450772b630f
Ismail Aatif <techcyber598@gmail.com>
2024-01-04 22:34:49 +0100

    updated Photo event

--- System/Events/Photo.php
+++ System/Events/Photo.php
@@ -4,6 +4,7 @@ namespace TeleBot\System\Events;
 
 use Attribute;
 use TeleBot\System\Interfaces\IEvent;
+use TeleBot\System\Types\IncomingPhoto;
 
 #[Attribute(Attribute::TARGET_METHOD)]
 class Photo implements IEvent
@@ -12,9 +13,14 @@ class Photo implements IEvent
     /**
      * @inheritDoc
      */
-    public function apply(array $event): bool
+    public function apply(array $event): array
     {
         $key = isset($event['data']['edited_message']) ? 'edited_message' : 'message';
-        return isset($event['data'][$key]) && isset($event['data'][$key]['photo']);
+        $photo = new IncomingPhoto($event['data'][$key]['photo']);
+
+        return [
+            isset($event['data'][$key]) && isset($event['data'][$key]['photo']),
+            $photo
+        ];
     }
 }
