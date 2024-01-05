2909d34befeb72ef523818590bf1849805f07928
ismailaatif <ismailaatif@oggvo.com>
2024-01-05 11:51:15 +0100

    update basic handler

--- App/Handlers/GetMyId.php
+++ App/Handlers/GetMyId.php
@@ -2,86 +2,27 @@
 
 namespace TeleBot\App\Handlers;
 
+use Exception;
 use TeleBot\System\BaseEvent;
-use TeleBot\System\Events\Audio;
-use TeleBot\System\Events\Contact;
-use TeleBot\System\Events\Dice;
-use TeleBot\System\Events\Document;
-use TeleBot\System\Events\Photo;
-use TeleBot\System\Events\Video;
-use TeleBot\System\Events\Voice;
-use TeleBot\System\Types\IncomingAudio;
-use TeleBot\System\Types\IncomingContact;
-use TeleBot\System\Types\IncomingDice;
-use TeleBot\System\Types\IncomingDocument;
-use TeleBot\System\Types\IncomingPhoto;
-use TeleBot\System\Types\IncomingVideo;
-use TeleBot\System\Types\IncomingVoice;
+use TeleBot\System\Events\Message;
 
 class GetMyId extends BaseEvent
 {
 
     /**
-     * handle incoming photo messages
+     * handle all incoming messages
      *
-     * @param IncomingPhoto $photo
      * @return void
+     * @throws Exception
      */
-    #[Photo]
-    public function handlePhoto(IncomingPhoto $photo): void {}
+    #[Message]
+    public function handle(): void
+    {
+        $userId = $this->event['message']['from']['id'];
+        $reply = "Your user ID: <strong>{$userId}</strong>\n";
+        $reply .= "Current chat ID: <strong>{$userId}</strong>";
 
-    /**
-     * handle incoming video messages
-     *
-     * @param IncomingVideo $video
-     * @return void
-     */
-    #[Video]
-    public function handleVideo(IncomingVideo $video): void {}
-
-    /**
-     * handle incoming audio messages
-     *
-     * @param IncomingAudio $audio
-     * @return void
-     */
-    #[Audio]
-    public function handleAudio(IncomingAudio $audio): void {}
-
-    /**
-     * handle incoming voice messages
-     *
-     * @param IncomingVoice $voice
-     * @return void
-     */
-    #[Voice]
-    public function handleVoice(IncomingVoice $voice): void {}
-
-    /**
-     * handle incoming document messages
-     *
-     * @param IncomingDocument $document
-     * @return void
-     */
-    #[Document]
-    public function handleDocument(IncomingDocument $document): void {}
-
-    /**
-     * handle incoming contact messages
-     *
-     * @param IncomingContact $contact
-     * @return void
-     */
-    #[Contact]
-    public function handleContact(IncomingContact $contact): void {}
-
-    /**
-     * handle incoming dice messages
-     *
-     * @param IncomingDice $dice
-     * @return void
-     */
-    #[Dice]
-    public function handleDice(IncomingDice $dice): void {}
+        $this->telegram->sendMessage($reply, false);
+    }
 
 }
