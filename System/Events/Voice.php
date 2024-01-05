da1e6806ec98be9b7c785ebb53d2d4722c17a1be
ismailaatif <ismailaatif@oggvo.com>
2024-01-05 10:12:03 +0100

    updated Voice/IncomingVoice

--- System/Events/Voice.php
+++ System/Events/Voice.php
@@ -4,6 +4,7 @@ namespace TeleBot\System\Events;
 
 use Attribute;
 use TeleBot\System\Interfaces\IEvent;
+use TeleBot\System\Types\IncomingVoice;
 
 #[Attribute(Attribute::TARGET_METHOD)]
 class Voice implements IEvent
@@ -12,9 +13,12 @@ class Voice implements IEvent
     /**
      * @inheritDoc
      */
-    public function apply(array $event): bool
+    public function apply(array $event): IncomingVoice|bool
     {
         $key = isset($event['data']['edited_message']) ? 'edited_message' : 'message';
-        return isset($event['data'][$key]) && isset($event['data'][$key]['voice']);
+        $isVoice = isset($event['data'][$key]) && isset($event['data'][$key]['voice']);
+        if (!$isVoice) return false;
+
+        return new IncomingVoice($event['data'][$key]['voice']);
     }
 }
--- System/Types/IncomingVoice.php
+++ System/Types/IncomingVoice.php
@@ -2,4 +2,66 @@
 
 namespace TeleBot\System\Types;
 
-class IncomingVoice extends File {}
+class IncomingVoice extends File
+{
+
+    /**
+     * get file id by index
+     *
+     * @return string
+     */
+    public function getFileId(): string
+    {
+        return $this->file['file_id'];
+    }
+
+    /**
+     * get voice duration in seconds
+     *
+     * @return int
+     */
+    public function getDuration(): int
+    {
+        return $this->file['duration'];
+    }
+
+    /**
+     * get voice mime type
+     *
+     * @return string
+     */
+    public function getMimeType(): string
+    {
+        return $this->file['mime_type'];
+    }
+
+    /**
+     * get voice file size
+     *
+     * @param bool $readable
+     * @return int|string
+     */
+    public function getSize(bool $readable = false): int|string
+    {
+        if (!$readable) return $this->file['file_size'];
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        $fileSize = $this->file['file_size'];
+        $n = 0;
+
+        while ($fileSize >= 1024 && $n++ < count($units))
+            $fileSize /= 1024;
+        return join(' ', [number_format($fileSize, 2), $units[$n]]);
+    }
+
+    /**
+     * download voice
+     *
+     * @return string|null returns stored file name
+     */
+    public function save(): ?string
+    {
+        $this->getLink($this->getFileId());
+        return $this->saveAs();
+    }
+
+}
