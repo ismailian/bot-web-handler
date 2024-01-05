a678b6656026b057242ab02dda8c41339b00940f
ismailaatif <ismailaatif@oggvo.com>
2024-01-05 10:48:41 +0100

    updated Document/IncomingDocument

--- System/Events/Document.php
+++ System/Events/Document.php
@@ -4,6 +4,7 @@ namespace TeleBot\System\Events;
 
 use Attribute;
 use TeleBot\System\Interfaces\IEvent;
+use TeleBot\System\Types\IncomingDocument;
 
 #[Attribute(Attribute::TARGET_METHOD)]
 class Document implements IEvent
@@ -12,9 +13,12 @@ class Document implements IEvent
     /**
      * @inheritDoc
      */
-    public function apply(array $event): bool
+    public function apply(array $event): IncomingDocument|bool
     {
         $key = isset($event['data']['edited_message']) ? 'edited_message' : 'message';
-        return isset($event['data'][$key]) && isset($event['data'][$key]['document']);
+        $isDocument = isset($event['data'][$key]) && isset($event['data'][$key]['document']);
+        if (!$isDocument) return false;
+
+        return new IncomingDocument($event['data'][$key]['document']);
     }
 }
--- System/Types/IncomingDocument.php
+++ System/Types/IncomingDocument.php
@@ -2,4 +2,76 @@
 
 namespace TeleBot\System\Types;
 
-class IncomingDocument extends File {}
+class IncomingDocument extends File
+{
+
+    /**
+     * get file id
+     *
+     * @return string
+     */
+    public function getFileId(): string
+    {
+        return $this->file['file_id'];
+    }
+
+    /**
+     * get document file name
+     *
+     * @return string
+     */
+    public function getFilename(): string
+    {
+        return $this->file['file_name'];
+    }
+
+    /**
+     * get document mime type
+     *
+     * @return string
+     */
+    public function getMimeType(): string
+    {
+        return $this->file['mime_type'];
+    }
+
+    /**
+     * get document file size
+     *
+     * @param bool $readable
+     * @return int|string
+     */
+    public function getSize(bool $readable = false): int|string
+    {
+        if (!$readable) return $this->file['file_size'];
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        $fileSize = $this->file['file_size'];
+        $n = 0;
+
+        while ($fileSize >= 1024 && $n++ < count($units))
+            $fileSize /= 1024;
+        return join(' ', [number_format($fileSize, 2), $units[$n]]);
+    }
+
+    /**
+     * get document thumbnail
+     *
+     * @return Thumbnail
+     */
+    public function getThumbnail(): Thumbnail
+    {
+        return new Thumbnail($this->file['thumbnail']);
+    }
+
+    /**
+     * download document
+     *
+     * @return string|null returns stored file name
+     */
+    public function save(): ?string
+    {
+        $this->getLink($this->getFileId());
+        return $this->saveAs();
+    }
+
+}
