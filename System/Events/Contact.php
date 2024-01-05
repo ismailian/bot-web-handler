4159d7c913877966d667931964d140d20b742969
ismailaatif <ismailaatif@oggvo.com>
2024-01-05 10:49:04 +0100

    updated Contact/IncomingContact

--- System/Events/Contact.php
+++ System/Events/Contact.php
@@ -4,6 +4,7 @@ namespace TeleBot\System\Events;
 
 use Attribute;
 use TeleBot\System\Interfaces\IEvent;
+use TeleBot\System\Types\IncomingContact;
 
 #[Attribute(Attribute::TARGET_METHOD)]
 class Contact implements IEvent
@@ -12,9 +13,12 @@ class Contact implements IEvent
     /**
      * @inheritDoc
      */
-    public function apply(array $event): bool
+    public function apply(array $event): IncomingContact|bool
     {
         $key = isset($event['data']['edited_message']) ? 'edited_message' : 'message';
-        return isset($event['data'][$key]) && isset($event['data'][$key]['contact']);
+        $isContact = isset($event['data'][$key]) && isset($event['data'][$key]['contact']);
+        if (!$isContact) return false;
+
+        return new IncomingContact($event['data'][$key]['contact']);
     }
 }
--- System/Types/IncomingContact.php
+++ System/Types/IncomingContact.php
@@ -2,4 +2,55 @@
 
 namespace TeleBot\System\Types;
 
-class IncomingContact extends File {}
+class IncomingContact
+{
+
+    /** @var array $contact */
+    protected array $contact;
+
+    public function __construct(array $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * get contact phone number
+     *
+     * @return string
+     */
+    public function getPhoneNumber(): string
+    {
+        return $this->contact['phone_number'];
+    }
+
+    /**
+     * get contact first name
+     *
+     * @return string|null
+     */
+    public function getFirstName(): ?string
+    {
+        return $this->contact['first_name'] ?? null;
+    }
+
+    /**
+     * get contact last name
+     *
+     * @return string|null
+     */
+    public function getLastName(): ?string
+    {
+        return $this->contact['last_name'] ?? null;
+    }
+
+    /**
+     * get contact vcard
+     *
+     * @return string
+     */
+    public function getVCard(): string
+    {
+        return $this->contact['vcard'];
+    }
+
+}
