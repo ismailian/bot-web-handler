4e41a46e5e76b510dadecc3947b8e6e02aa6c07d
ismailaatif <ismailaatif@oggvo.com>
2024-01-05 10:49:38 +0100

    updated basic handler

--- App/Handlers/GetMyId.php
+++ App/Handlers/GetMyId.php
@@ -4,10 +4,16 @@ namespace TeleBot\App\Handlers;
 
 use TeleBot\System\BaseEvent;
 use TeleBot\System\Events\Audio;
+use TeleBot\System\Events\Contact;
+use TeleBot\System\Events\Dice;
+use TeleBot\System\Events\Document;
 use TeleBot\System\Events\Photo;
 use TeleBot\System\Events\Video;
 use TeleBot\System\Events\Voice;
 use TeleBot\System\Types\IncomingAudio;
+use TeleBot\System\Types\IncomingContact;
+use TeleBot\System\Types\IncomingDice;
+use TeleBot\System\Types\IncomingDocument;
 use TeleBot\System\Types\IncomingPhoto;
 use TeleBot\System\Types\IncomingVideo;
 use TeleBot\System\Types\IncomingVoice;
@@ -50,5 +56,32 @@ class GetMyId extends BaseEvent
      */
     #[Voice]
     public function handleVoice(IncomingVoice $voice): void {}
+
+    /**
+     * handle incoming document messages
+     *
+     * @param IncomingDocument $document
+     * @return void
+     */
+    #[Document]
+    public function handleDocument(IncomingDocument $document): void {}
+
+    /**
+     * handle incoming contact messages
+     *
+     * @param IncomingContact $contact
+     * @return void
+     */
+    #[Contact]
+    public function handleContact(IncomingContact $contact): void {}
+
+    /**
+     * handle incoming dice messages
+     *
+     * @param IncomingDice $dice
+     * @return void
+     */
+    #[Dice]
+    public function handleDice(IncomingDice $dice): void {}
 
 }
