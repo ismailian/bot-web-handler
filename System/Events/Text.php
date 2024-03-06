cadcbfb8187f4e6698e9da004409a296b56a1cff
ismailaatif <ismailaatif@oggvo.com>
2024-03-06 17:53:54 +0100

    update Text event

--- System/Events/Text.php
+++ System/Events/Text.php
@@ -4,6 +4,7 @@ namespace TeleBot\System\Events;
 
 use Attribute;
 use TeleBot\System\Interfaces\IEvent;
+use TeleBot\System\Interfaces\IValidator;
 
 #[Attribute(Attribute::TARGET_METHOD)]
 class Text implements IEvent
@@ -12,10 +13,10 @@ class Text implements IEvent
     /**
      * default constructor
      *
-     * @param bool $textOnly capture plain-text only
+     * @param bool $cleanText capture plain-text only
      * this will only capture text messages without mentions, urls or commands
      */
-    public function __construct(public bool $textOnly = false) {}
+    public function __construct(public bool $cleanText = false, public ?IValidator $Validator = null) {}
 
     /**
      * @inheritDoc
@@ -27,9 +28,11 @@ class Text implements IEvent
         $hasText = isset($event['data'][$key]['text']);
         if (!$isMessage || !$hasText) return false;
 
-        return !$this->textOnly || !count(array_filter(
-            $event['data'][$key]['entities'] ?? [],
-            fn($entity) => in_array($entity['type'], ['bot_command', 'url', 'mention'])
-        ));
+        $isCleanText = !$this->cleanText || !count(array_filter(
+                $event['data'][$key]['entities'] ?? [],
+                fn($entity) => in_array($entity['type'], ['bot_command', 'url', 'mention'])
+            ));
+
+        return $isCleanText && $this->Validator->isValid($event['data'][$key]['text']);
     }
 }
