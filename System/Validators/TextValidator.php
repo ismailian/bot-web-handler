82c6c3da42d74c3c72b940d1b276b2e1eb3515ae
ismailaatif <ismailaatif@oggvo.com>
2024-03-08 15:58:50 +0100

    update TextValidator

--- System/Validators/TextValidator.php
+++ System/Validators/TextValidator.php
@@ -11,21 +11,25 @@ class TextValidator implements IValidator
      * default constructor
      *
      * @param int|null $minLength
-     * @param int|null $equals
+     * @param string|null $equals
      * @param string|null $regex
      */
-    public function __construct(public ?int $minLength = null, public ?int $equals = null, public ?string $regex = null) {}
+    public function __construct(
+        public ?int    $minLength = null,
+        public ?string $equals = null,
+        public ?string $regex = null
+    ) {}
 
     /**
      * @inheritDoc
      */
     public function isValid(mixed $data): bool
     {
-        if ($this->minLength && $this->minLength > strlen($data)) return false;
-        if ($this->equals && $this->equals !== $data) return false;
-        if ($this->regex && !((bool)preg_match("/{$this->regex}/i", $data)))
-            return false;
-
+        if (
+            ($this->minLength && $this->minLength > strlen($data))
+            || ($this->equals && $this->equals !== $data)
+            || ($this->regex && !((bool)preg_match("/{$this->regex}/i", $data)))
+        ) return false;
         return true;
     }
 }
