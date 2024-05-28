195000f48f6b423f3dfa96ac16c652dc11846121
Ismail Aatif <techcyber598@gmail.com>
2024-05-28 23:36:08 +0100

    update CallbackQuery

--- System/Telegram/Types/CallbackQuery.php
+++ System/Telegram/Types/CallbackQuery.php
@@ -8,11 +8,14 @@ class CallbackQuery
     /** @var string $id callback id */
     public string $id;
 
-    /** @var string $messageId source message id */
-    public string $messageId;
+    /** @var string $chatInstance chat instance */
+    public string $chatInstance;
 
     /** @var From $from sender */
     public From $from;
+
+    /** @var Message $message callback message */
+    public Message $message;
 
     /** @var mixed query data */
     public mixed $data;
@@ -25,8 +28,10 @@ class CallbackQuery
     public function __construct(protected array $callback)
     {
         $this->id = $this->callback['id'];
-        $this->messageId = $this->callback['inline_message_id'];
+        $this->chatInstance = $this->callback['chat_instance'];
+
         $this->from = new From($this->callback['from']);
+        $this->message = new Message($this->callback['message']);
         $this->data = $this->callback['data'];
 
         if (($json = json_decode($this->data, true))) {
