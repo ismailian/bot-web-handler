8a523aba2703778aea2c27dc11e55a2a3c452b5a
ismailaatif <ismailaatif@oggvo.com>
2024-06-03 15:15:36 +0100

    add SuccessfulPayment event + type

---
+++ System/Telegram/Events/SuccessfulPayment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TeleBot\System\Telegram\Events;
+
+use Attribute;
+use TeleBot\System\Interfaces\IEvent;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+class SuccessfulPayment implements IEvent
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function apply(array $event): bool
+    {
+        return false;
+    }
+}
