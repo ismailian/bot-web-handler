54fce00543912dcd56a36955161742dcc6c89515
ismailaatif <ismailaatif@oggvo.com>
2024-10-21 11:16:50 +0100

    Update IncomingRequest.php

--- System/IncomingRequest.php
+++ System/IncomingRequest.php
@@ -10,32 +10,22 @@
 
 namespace TeleBot\System;
 
-use TeleBot\System\Http\Request;
-use TeleBot\System\Http\Response;
 use TeleBot\System\Telegram\BotApi;
 
 class IncomingRequest
 {
 
-    /** @var Request $request http request */
-    protected Request $request;
-
-    /** @var Response $response */
-    protected Response $response;
+    /** @var array $config */
+    public array $config = [];
 
     /** @var BotApi $telegram telegram client */
     protected BotApi $telegram;
-
-    /** @var array $config */
-    public array $config = [];
 
     /**
      * default constructor
      */
     public function __construct()
     {
-        $this->request = new Request();
-        $this->response = new Response();
         $this->telegram = new BotApi();
     }
 
