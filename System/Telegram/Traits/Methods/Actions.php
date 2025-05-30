5ab9f48a188f673a989f0c0321a300e458c9e595
Ismail Aatif <techcyber598@gmail.com>
2025-05-30 14:49:55 +0000

    fix chat actions bug

--- System/Telegram/Traits/Methods/Actions.php
+++ System/Telegram/Traits/Methods/Actions.php
@@ -11,34 +11,23 @@
 namespace TeleBot\System\Telegram\Traits\Methods;
 
 use TeleBot\System\Telegram\BotApi;
+use TeleBot\System\Telegram\Enums\ChatActions;
 use TeleBot\System\Telegram\Traits\Extensions;
 
 trait Actions
 {
 
-    const TYPING = 'typing';
-    const UPLOAD_PHOTO = 'upload_photo';
-    const UPLOAD_VIDEO = 'upload_video';
-    const RECORD_VIDEO = 'record_video';
-    const RECORD_VIDEO_NOTE = 'record_video_note';
-    const UPLOAD_VIDEO_NOTE = 'upload_video_note';
-    const RECORD_VOICE = 'record_video';
-    const UPLOAD_VOICE = 'upload_voice';
-    const UPLOAD_DOCUMENT = 'upload_document';
-    const CHOOSE_STICKER = 'choose_sticker';
-    const FIND_LOCATION = 'find_location';
-
     /**
      * send an action
      *
-     * @param string $action action to send
+     * @param ChatActions $action action to send
      * @return BotApi|Extensions
      */
-    public function sendChatAction(string $action): self
+    public function sendChatAction(ChatActions $action = ChatActions::TYPING): self
     {
         $this->post(__FUNCTION__, [
             'chat_id' => $this->chatId,
-            'action' => $action
+            'action' => $action->value
         ]);
 
         return $this;
