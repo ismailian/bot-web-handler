334867670be5548130f32fec7f82cd7967cf92c9
Ismail Aatif <ismail.aatif@merkle.com>
2025-11-09 20:41:58 +0100

    feat: add expiration date to cache

--- System/Cache/Cache.php
+++ System/Cache/Cache.php
@@ -10,28 +10,17 @@
 
 namespace TeleBot\System\Cache;
 
-use TeleBot\System\Core\Traits\Cacheable;
+use TeleBot\System\Core\Traits\Expirable;
 use TeleBot\System\Interfaces\ICacheDriver;
 use TeleBot\System\Cache\Drivers\{FileDriver, RedisDriver};
 
 class Cache
 {
 
-    use Cacheable;
+    use Expirable;
 
     /** @var ICacheDriver|null $client client */
     protected ?ICacheDriver $client = null;
-
-    /**
-     * get cached data
-     *
-     * @param string $key
-     * @return mixed
-     */
-    public function get(string $key): mixed
-    {
-        return $this->init()->read($key);
-    }
 
     /**
      * initialize driver client
@@ -41,7 +30,7 @@ class Cache
     protected function init(): ICacheDriver
     {
         if ($this->client === null) {
-            $driver = env('CACHE_DRIVER');
+            $driver = env('CACHE_DRIVER', 'filesystem');
             $this->client = match ($driver) {
                 'redis' => new RedisDriver(),
                 'filesystem' => new FileDriver(),
@@ -52,14 +41,51 @@ class Cache
     }
 
     /**
+     * get request fingerprint
+     *
+     * @return string
+     */
+    public function fingerprint(): string
+    {
+        $ip = request()->ip();
+        $uri = request()->uri();
+        $method = request()->method();
+
+        return md5("$ip|$method|$uri");
+    }
+
+    /**
+     * get cached data
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function get(string $key): mixed
+    {
+        $data = $this->init()->read($key);
+        if ($this->isExpired($data)) {
+            $this->forget($key);
+            return null;
+        }
+
+        $data = $this->restore($data);
+
+        unset($data[$this->expireKey]);
+        return $data;
+    }
+
+    /**
      * set data
      *
      * @param string $key
      * @param mixed $value
+     * @param string|null $expires
      * @return bool
      */
-    public function remember(string $key, mixed $value): bool
+    public function remember(string $key, mixed $value, ?string $expires = null): bool
     {
+        $this->addExpireTimestamp($value, $expires);
+
         return $this->init()->write($key, $value);
     }
 
